7eed29bbdb25a563a032fcb83f1ede5a9897cc4b
madhurpandya <gang05885@gmail.com>
2024-09-16 10:44:50 +0530

    exam-madhur exam 2 mcq

---
+++ c_programming/exam/exam 2/madhur exam 2.clj
@@ -0,0 +1,42 @@
+ EXAM/02Exam gim
+
+
+
+(1)What is the missing number in the series: 3, 6, 9, 12, ?
+#ans  (b) 1
+(2) If Alice is taller than Bob and Bob is taller than Charlie, who is the shortest?
+#ans  (c) Charlie 
+(3) In a certain code, “HELLO” is written as “IFMMP”. How is “WORLD” written in that code?
+#ans  (b) XPSNF 
+(4) A man buys a watch for $60. He sells it at a loss of 20%. How much does he sell it for?
+#ans (a) $48
+(5) Which number is missing in the following series? 2, 4, 8, 16, ?, 64
+#ans (d) 54
+(7) Which letter comes next in the sequence: B, D, F, H, ?
+#ans (d) M
+(8) Find the next number in the sequence: 5, 10, 20, 40, ?
+#ans  (b) 60
+(9) A car travels 60 miles in 1 hour. How far will it travel in 3.5 hours at the same speed?
+#ans   (b) 210 miles 
+(10) Which of the following does not belong?
+#ans (c) Carrot
+(11) If a train departs at 3:15 PM and arrives at 6:45 PM, how long is the journey?
+#ans (b) 3 hours 30 minutes 
+(12) Which of the following is the odd one out?
+#ans (d) 15
+(13) If ‘KNIT’ is coded as ‘JMHF’, how is ‘SEW’ coded in the same code?
+#ans (c) REX
+(14) A shopkeeper buys a shirt for $40 and sells it for $60. What is the percentage profit?
+#ans  (a) 50% 
+(15) In a group of 50 people, 30 like coffee, 20 like tea, and 10 like both. How many people like either coffee or tea or both?
+#ans   (d) 70
+(16) Which number should replace the question mark in the following pattern: 3, 6, 12, 24, ?
+#ans (a) 36 
+(17) What is the next number in the sequence: 1, 4, 9, 16, 25, ?
+#ans (d) 40
+(18) What comes next in the sequence: 2, 5, 10, 17, ?
+#ans  (c) 30 
+(19) If John is twice as old as Mary and Mary is 8 years old, how old is John?
+#ans (c) 16 
+(20) In a family, there are two parents and three children. If the family has exactly 5 members, how many of the members are children?
+#ans (a) 2 
