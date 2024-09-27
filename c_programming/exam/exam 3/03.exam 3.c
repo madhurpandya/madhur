84a1c2c243a3144eb64489fab8f4a183a5a3002f
madhurpandya <gang05885@gmail.com>
2024-09-27 11:29:28 +0530

    exam-madhur 1d and2d eaxm 3

---
+++ c_programming/exam/exam 3/03.exam 3.c
@@ -0,0 +1,41 @@
+1. Which of the following is a valid C program?
+#ans (A) int main() { return 0; }
+2. Who developed the C programming language?
+#ans (A) Dennis Ritchie 
+3. Which of the following is NOT a basic data type in C?
+#ans (C) string
+4. What is the size of the int data type in C on a typical 32-bit system?
+#ans (B) 4 bytes
+5. Which of the following declares a constant variable in C?
+#ans  (D) All of the above
+6. Which operator is used to get the address of a variable in C?
+#ans (D) %
+7. What will the expression 5 + 2 * 3 evaluate to?
+#ans  (B) 11 
+8. Which of the following is the correct syntax for an if statement?
+#ans  if (x > 0) { }
+9. Which control structure allows for repeated execution of code while a condition is true?
+#ans (C) for loop 
+10. Which of the following loops is guaranteed to execute at least once?
+#ans (B) while loop 
+11. What is the output of the following code snippet?
+#ans (A) 0 1 2
+12. What will the following code output?
+#ans (a) * ** ***
+13. How do you declare an array of 10 integers in C?
+#ans (A) int array[10];
+14. What is the valid way to access the third element of an array named arr?
+#ans (A) arr[2] 
+15. Which of the following is a correct way to define a function in C?
+#ans (A) void functionName() {} 
+16. What is the return type of a function that does not return a value?
+#ans (C) null 
+17. Which keyword is used to call a function in C?
+#ans  (C) return
+18. What will the following function return?
+#ans (A) The sum of a and b
+19. What is the default return type of a function if not explicitly specified?
+#ans (A) int
+20. Which of the following correctly represents a pointer in C?
+#ans 
+#ans  (C) *int ptr; 
